43e27ad5b09fe5611e25c6565e1d9b9aed522cab
gddavies <george.davies@incrowdsports.com>
2018-03-02 18:28:19 +0000

    Added all networking calls and started to implement in VCs, also added tableview to accounts view

---
+++ MoneyApp/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  MoneyApp
+//
+//  Created by George Davies on 02/03/2018.
+//  Copyright © 2018 GeorgeDavies. All rights reserved.
+//
+
+import Foundation
+import MBProgressHUD
+
+extension UIViewController {
+    
+}
---
+++ MoneyApp/Product.swift
@@ -0,0 +1,50 @@
+//
+//  ThisWeekModel.swift
+//  MoneyApp
+//
+//  Created by George Davies on 02/03/2018.
+//  Copyright © 2018 GeorgeDavies. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    var name: String?
+    var productId: Int?
+    var productType: ProductType?
+    var moneybox: Double?
+    var subscriptionAmount: Double?
+    var planValue: Double?
+    var contributedYTD: Double?
+    var transferredInYTD: Double?
+    var maxDeposit: Double?
+    
+    init?(data: [String: Any]) {
+        guard
+            let product = data["Product"] as? [String: Any],
+            let moneybox = data["Moneybox"],
+            let subscriptionAmount = data["SubscriptionAmount"],
+            let planValue = data["PlanValue"],
+            let productType = data["InvestorProductType"] as? String
+            else { return }
+        
+        self.name = product["FriendlyName"] as? String
+        self.productId = data["InvestorProductId"] as? Int
+        self.productType = ProductType(rawValue: productType)
+        self.moneybox = moneybox as? Double
+        self.subscriptionAmount = subscriptionAmount as? Double
+        self.planValue = planValue as? Double
+        self.contributedYTD = data["Sytd"] as? Double
+        self.transferredInYTD = data["TransferInSytd"] as? Double
+        self.maxDeposit = data["MaximumDeposit"] as? Double
+    }
+    
+    static func convertToCurrency(amount: Double) -> String? {
+        return ""
+    }
+}
+
+enum ProductType: String {
+    case isa = "Isa"
+    case gia = "Gia"
+}
