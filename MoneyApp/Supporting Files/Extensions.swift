329c538f191ef6d78d609c226bb3563be07951d8
gddavies <george.davies@incrowdsports.com>
2018-03-04 17:27:46 +0000

    Updated validation and UI tweaks

--- MoneyApp/Supporting Files/Extensions.swift
+++ MoneyApp/Supporting Files/Extensions.swift
@@ -28,3 +28,11 @@ extension UIImageView {
         layer.borderColor = UIColor.black.cgColor
     }
 }
+
+extension UITextField {
+    func setLeftPadding(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+}
